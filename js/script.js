affa93707af4f1721b72bb6fb6bc844586cbafc0
Brian Garcia <brian.garcia@student.uncommonhs.org>
2018-03-15 22:18:16 +0000

    Changed song names

--- js/script.js
+++ js/script.js
@@ -17,30 +17,31 @@
 
 // Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"title":"God's plan",
+	"artist":"Drake ",
+	"songLength":"3:20"
+	"mp3url":"https://www.youtube.com/watch?v=xpVfcZ0ZcFM",
+	"imageurl":"https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2F91dfa9a875349f2c1a4c8c6420642c04.600x600x1.jpg",
 }
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"title":"God's plan",
+	"artist":"Drake ",
+	"mp3url":"https://www.youtube.com/watch?v=xpVfcZ0ZcFM",
+	"imageurl":"https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2F91dfa9a875349f2c1a4c8c6420642c04.600x600x1.jpg",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"Havana",
+		"artist":"Camila Cabello",
+		"mp3url":"https://www.youtube.com/watch?v=zDIAam5qGUc",
+		"imageurl":"https://i.ytimg.com/vi/HCjNJDNzw8Y/maxresdefault.jpg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title":"Mine",
+		"artist":"Bazzi",
+		"mp3url":"https://www.youtube.com/watch?v=Gc71AmT_b2k",
+		"imageurl":"https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2F56646a433da886085362eca46e1f03d5.1000x1000x1.jpg",
 	}
 
 ]
